b762fa7a1019ad8cf3c37c09f5c81132066751e8
Ariel Lothlorien <git@arielkauai.com>
2015-10-08 01:18:56 -1000

    Added some basic roll buttons to the Dice Roller
    Signed-off-by: Ariel Lothlorien <git@arielkauai.com>

--- src/javascripts/ui/DiceRoller.jsx
+++ src/javascripts/ui/DiceRoller.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import FloatingActionButton from 'material-ui/lib/floating-action-button';
+
 //import { Router, Route, Link } from 'react-router'
 
 export default class DiceRoller extends React.Component {
@@ -7,8 +9,30 @@ export default class DiceRoller extends React.Component {
     //this.state = { n: 0 }
   }
   render () {
+  	var style={
+  		color: "#8200D4",
+  		fontWeight: "bold"
+  	}
+
     return <div>
-      <h1>clicked times</h1>
+		<FloatingActionButton style={style} backgroundColor="#52D400" mini={true}>
+			d2
+		</FloatingActionButton>
+		<FloatingActionButton style={style} backgroundColor="#52D400" mini={true}>
+			d4
+		</FloatingActionButton>
+		<FloatingActionButton style={style} backgroundColor="#52D400" mini={true}>
+			d6
+		</FloatingActionButton>
+		<FloatingActionButton style={style} backgroundColor="#52D400" mini={true}>
+			d8
+		</FloatingActionButton>
+		<FloatingActionButton style={style} backgroundColor="#52D400" mini={true}>
+			d12
+		</FloatingActionButton>
+		<FloatingActionButton style={style} backgroundColor="#52D400" mini={true}>
+			d20
+		</FloatingActionButton>
     </div>
   }
 }
