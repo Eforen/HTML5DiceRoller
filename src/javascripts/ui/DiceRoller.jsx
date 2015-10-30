05526843f25a9bc0ba18174be506b82b22d249d1
Ariel Lothlorien <git@arielkauai.com>
2015-10-30 01:45:30 -1000

    Forgot to add these files
    Signed-off-by: Ariel Lothlorien <git@arielkauai.com>

--- src/javascripts/ui/DiceRoller.jsx
+++ src/javascripts/ui/DiceRoller.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import FloatingActionButton from 'material-ui/lib/floating-action-button';
+import { Button , ButtonGroup, Input, Glyphicon} from 'react-bootstrap';
 
 //import { Router, Route, Link } from 'react-router'
 
@@ -7,32 +8,85 @@ export default class DiceRoller extends React.Component {
   constructor(props, context) {
     super(props, context)
     //this.state = { n: 0 }
+    this.state = {
+    	code: "d6",
+    	dieBtn: 2,
+    	btns: ["d2", "d4", "d6", "d8", "d10", "d12", "d20", "d100"],
+    	feature: {}
+    }
   }
+
+  btnClicked(index){
+  	this.setState({"dieBtn":index})
+  	//alert(this.state.dieBtn+"|"+index)
+  }
+
+  checkState(self, id){
+  	return self.state.dieBtn == id
+  }
+
+  featureToggle(feature){
+  	if (this.state.feature[feature] == true) this.state.feature[feature]=false
+  	else {
+	  	switch(feature){
+	  		case "Drop Low":
+	  			this.state.feature["Add Low"] = false
+	  		case "Add Low":
+	  			this.state.feature["Drop Low"] = false
+	  		case "Drop High":
+	  			this.state.feature["Add High"] = false
+	  		case "Add High":
+	  			this.state.feature["Drop High"] = false
+	  	}
+	  	this.state.feature[feature] = true
+  	}
+  	this.setState({"feature":this.state.feature})
+  }
+
+  checkFeature(self, feature){
+  	if(self.state.feature[feature]===true) return true
+  	return false
+  }
+
   render () {
   	var style={
   		color: "#8200D4",
   		fontWeight: "bold"
   	}
 
-    return <div>
-		<FloatingActionButton style={style} backgroundColor="#52D400" mini={true}>
-			d2
-		</FloatingActionButton>
-		<FloatingActionButton style={style} backgroundColor="#52D400" mini={true}>
-			d4
-		</FloatingActionButton>
-		<FloatingActionButton style={style} backgroundColor="#52D400" mini={true}>
-			d6
-		</FloatingActionButton>
-		<FloatingActionButton style={style} backgroundColor="#52D400" mini={true}>
-			d8
-		</FloatingActionButton>
-		<FloatingActionButton style={style} backgroundColor="#52D400" mini={true}>
-			d12
-		</FloatingActionButton>
-		<FloatingActionButton style={style} backgroundColor="#52D400" mini={true}>
-			d20
-		</FloatingActionButton>
-    </div>
+  	var btnCountDec = <Button><Glyphicon glyph="glyphicons-minus" /></Button>
+  	var btnCountInc = <Button><Glyphicon glyph="glyphicons-plus" /></Button>
+
+  	var current = (
+  		<div className="DiceRoller">
+<Glyphicon glyph="glyphicons-minus" /> <Glyphicon glyph="glyphicons-plus" />
+    		<Input className="DiceCountSelect" type="number" min={1} buttonBefore={btnCountDec} buttonAfter={btnCountInc} />
+	  		<ButtonGroup className="DiceSideSelect">
+		        {this.state.btns.map(function(btn, i) {
+		          var boundClick = this.btnClicked.bind(this, i);
+		          return (
+		          	<Button bsSize="large" key={i} componentClass="DiceSideBtn" active={this.checkState(this, i)} onClick={boundClick} >{btn}</Button>
+		          );
+		        }, this)}
+	  		</ButtonGroup>
+	  		<ButtonGroup className="DiceFeatureSelect1">
+		        {["Reroll Ones", "Extra Dice"].map(function(feature, i) {
+		          var boundClick = this.featureToggle.bind(this, feature);
+		          return (
+		          	<Button bsSize="large" key={"features1-"+i} componentClass="DiceSideBtn" active={this.checkFeature(this, feature)} onClick={boundClick} >{feature}</Button>
+		          );
+		        }, this)}
+	  		</ButtonGroup>
+	  		<ButtonGroup className="DiceFeatureSelect2">
+		        {["Drop Low", "Add Low", "Drop High", "Add High"].map(function(feature, i) {
+		          var boundClick = this.featureToggle.bind(this, feature);
+		          return (
+		          	<Button bsSize="large" key={"features2-"+i} componentClass="DiceSideBtn" active={this.checkFeature(this, feature)} onClick={boundClick} >{feature}</Button>
+		          );
+		        }, this)}
+	  		</ButtonGroup>
+  		</div>)
+
+    return current
   }
 }
