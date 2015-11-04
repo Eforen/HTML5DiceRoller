fd82443e50eb8ba459d77c271a61f00afbde4d73
Ariel Lothlorien <git@arielkauai.com>
2015-11-03 16:45:33 -1000

    MsgConstants.js fixed to only es6

--- src/javascripts/data/constants/MsgConstants.js
+++ src/javascripts/data/constants/MsgConstants.js
@@ -1,7 +1,7 @@
-export var ROLE_CREATE = Symbol('RollStore-create');
-export var ROLE_UPDATE = Symbol('RollStore-update');
+export const ROLE_CREATE = Symbol('RollStore-create');
+export const ROLE_UPDATE = Symbol('RollStore-update');
 
-export var SESSION_CREATE = Symbol('SessionStore-create');
-export var SESSION_UPDATE = Symbol('SessionStore-update');
-export var SESSION_JOIN = Symbol('SessionStore-join');
-export var SESSION_LEAVE = Symbol('SessionStore-leave');
+export const SESSION_CREATE = Symbol('SessionStore-create');
+export const SESSION_UPDATE = Symbol('SessionStore-update');
+export const SESSION_JOIN = Symbol('SessionStore-join');
+export const SESSION_LEAVE = Symbol('SessionStore-leave');
